ad39f2d51df6b7d64a9aeb95719f4770eb91a8a2
JOPA167 <joparajirj@gmail.com>
2025-12-17 12:51:24 -0500

    2do commit

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -13,14 +13,43 @@ import java.util.Scanner;
  */
 public class Ejercicio {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
         String[] estaciones = new String[3];
-        int[][] produccion = new int[5][12];
+        int[][] producciones = new int[5][12];
+        
+        int[] sumaTotal = new int[5];
+
+        int suma = 0;
+        String estacion;
+        int produccion;
 
         System.out.println(
                 "ANÁLISIS DE PRODUCCIÓN");
 
+        for (int i = 0; i < estaciones.length; i++){
+            suma = 0;
+            entrada.nextLine();
+            System.out.println("Ingrese el nombre de la Estación");
+            estaciones[i] = entrada.nextLine();
+            for (int c = 0; c < 12; c++) {
+                System.out.printf("Ingrese la producción del mes %d de la estación %s",
+                        c +1 ,
+                        estaciones[i]);
+                producciones[i][c] = entrada.nextInt();
+                suma = suma + producciones[i][c];
+                
+
+            }
+            sumaTotal[i] = suma;
+
+        }
+        
+        for (int i = 0; i < estaciones.length; i++){
+        System.out.printf("ESTACIÓN 1: %s - TOTAL PRODUCCIÓN: %d",
+                estaciones[i],
+                sumaTotal[i]);
+        }
     }
 }

2358ed926ff7786e7100879b6c91d27d2ec21907
JOPA167 <joparajirj@gmail.com>
2025-12-17 12:31:33 -0500

    1er commit

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -4,10 +4,23 @@
  */
 package paquete05;
 
+//import. java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author UTPL
  */
 public class Ejercicio {
-    
+
+    public static void main(String[]args) {
+
+        Scanner entrada = new Scanner(System.in);
+        String[] estaciones = new String[3];
+        int[][] produccion = new int[5][12];
+
+        System.out.println(
+                "ANÁLISIS DE PRODUCCIÓN");
+
+    }
 }

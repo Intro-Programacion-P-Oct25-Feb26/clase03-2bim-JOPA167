f7e4265fd66306195d9c7e31ffa488f4c5b6f083
JOPA167 <joparajirj@gmail.com>
2025-12-17 19:20:15 -0500

    Solución problema paquete 5

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -16,23 +16,29 @@ public class Ejercicio {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
-        String[] estaciones = new String[3];
+        String[] estaciones = new String[5];
+        String[] encargados = new String[5];
+        int destacado = 0;
+        int mayorPro = 0;
+        
+        String reporte = "";
         int[][] producciones = new int[5][12];
         
+        
         int[] sumaTotal = new int[5];
-
         int suma = 0;
-        String estacion;
-        int produccion;
-
+        
         System.out.println(
                 "ANÁLISIS DE PRODUCCIÓN");
 
         for (int i = 0; i < estaciones.length; i++){
             suma = 0;
-            entrada.nextLine();
-            System.out.println("Ingrese el nombre de la Estación");
+            
+            System.out.printf("Ingrese el nombre de la Estación %d: ",i+1);
             estaciones[i] = entrada.nextLine();
+            System.out.println("Ingrese el nombre del encargado: ");
+            encargados[i] = entrada.nextLine();
+            
             for (int c = 0; c < 12; c++) {
                 System.out.printf("Ingrese la producción del mes %d de la estación %s",
                         c +1 ,
@@ -43,13 +49,32 @@ public class Ejercicio {
 
             }
             sumaTotal[i] = suma;
+            entrada.nextLine();
 
         }
         
         for (int i = 0; i < estaciones.length; i++){
-        System.out.printf("ESTACIÓN 1: %s - TOTAL PRODUCCIÓN: %d",
+        reporte = String.format("%sESTACIÓN %d: %s - TOTAL PRODUCCIÓN: %d\n",
+                reporte,
+                i+1,
                 estaciones[i],
                 sumaTotal[i]);
         }
+        
+        
+        for(int i = 0; i<5;i++){
+            if (sumaTotal[i] > mayorPro){
+                mayorPro= sumaTotal[i];
+                destacado = i;
+            }
+        }
+        reporte = String.format("%s\nEstación más Productiva:%s\n"
+                + "Encargado de la Estación: %s\n"
+                + "Cantidad de la Estación más Productiva: %d",
+                reporte,
+                estaciones[destacado],
+                encargados[destacado],
+                sumaTotal[destacado]);
+        System.out.printf("%s",reporte);
     }
 }
